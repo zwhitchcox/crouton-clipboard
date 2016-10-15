b8f9854b9b9d31871a27e8d69075aa7f996b0dc2
Zane Hitchcox <zwhitchcox@gmail.com>
2016-10-15 12:34:16 -0400

    auto start server

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 git clone http://github.com/zwhitchcox/crouton-clipboard ~/.crouton-clipboard
 echo "node ~/.crouton-clipboard/server.js &" >> $HOME/.bashrc
 echo "nnoremap \"*p :r !cat $HOME/.crouton-clipboard/data.txt<CR>\nvnoremap \"*y :'<,'>w! $HOME/.crouton-clipboard/data.txt<CR>" >> ~/.vimrc
+node ~/.crouton-clipboard/server &
